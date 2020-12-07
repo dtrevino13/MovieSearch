b1a2e47dad76622ca24f6fd98d05f0619f5eea04
dtrevino13 <58088362+dtrevino13@users.noreply.github.com>
2020-12-07 13:08:18 -0500

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -18,29 +18,5 @@ public:
     string getGenre();
     string getId();
 };
-Movie::Movie() {}
-
-Movie::Movie(string t, string g, string y, string i) {
-    title = t;
-    genre = g;
-    year = y;
-    id = i;
-}
-
-string Movie::getTitle() {
-    return title;
-}
-
-string Movie::getYear() {
-    return year;
-}
-
-string Movie::getGenre() {
-    return genre;
-}
-
-string Movie::getId() {
-    return id;
-}
 
 #endif //ULTIMATEMOVIESEARCH_MOVIE_H
